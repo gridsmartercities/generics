c0276bf3b240b781969f305b2019631f140bcdd4
jonathan hamilton <jonathanhamilton@jonathans-MacBook-Pro-2.local>
2019-07-01 15:52:15 +0100

    added tests for buttons

---
+++ src/components/generics/button/button.test.js
@@ -0,0 +1,32 @@
+import "jest-dom/extend-expect";
+import React from "react";
+import { Simulate } from "react-dom/test-utils";
+import { render } from "@testing-library/react";
+import Button from "./index";
+import theme from "../../../style/theme";
+import { ThemeProvider } from "styled-components";
+
+test("Regular Button Component", () => {
+  const { getByLabelText, container } = render(
+    <ThemeProvider theme={theme}>
+      <Button>Hello</Button>
+    </ThemeProvider>
+  );
+  const button = document.querySelector("button");
+  expect(button).toHaveTextContent("Hello");
+  expect(container.firstChild).toMatchSnapshot();
+});
+
+test("Link Button Component", () => {
+  const { getByLabelText, container } = render(
+    <ThemeProvider theme={theme}>
+      <Button href="/hello">Hello</Button>
+    </ThemeProvider>
+  );
+
+  const link = document.querySelector("a");
+  expect(link.nodeName).toBe("A");
+  expect(link).toHaveTextContent("Hello");
+  expect(link.href).toEqual("http://localhost/hello");
+  expect(container.firstChild).toMatchSnapshot();
+});
